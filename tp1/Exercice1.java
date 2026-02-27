61919a2d76b72f46d1f867656c68e93b4c041d7a
hajarmst211 <hajarmst21@gmail.com>
2026-02-27 10:48:39 +0000

    added a main

--- tp1/Exercice1.java
+++ tp1/Exercice1.java
@@ -1,27 +1,36 @@
-
-
 class increment_int implements Runnable{
+    public int final_int;
+
     public void run(){
         for(int compteur_int = 0; compteur_int < 10; compteur_int++){
             System.out.println("Comptuer int is at:"+ compteur_int);
+            final_int ++;
         }
+    }
+
+    public int getResult(){
+        return final_int;
     }
 }
 
 class increment_char implements Runnable{
+    public char final_char;
+
     public void run(){
-        for (char character = 'a'; character <= 'z'; character ++)
+        for (char character = 'a'; character <= 'z'; character ++){
             System.out.println("the charcter now is:"+ character);
+            final_char = character;
         }
+    }
+
+    public char getResult(){
+        return final_char;
+    }
 }
 
-public class Compteur {
+public class Exercice1 {
     public static void main(String[] args) {
         
-        for (int i=0; i<7 ; i++){System.out.print("_");}
-        System.out.print("Exercice 1:");
-        for (int i=0; i<7 ; i++){System.out.print("_");}
-
         increment_int task1 = new increment_int();
         increment_char task2 = new increment_char();
 
@@ -30,6 +39,13 @@ public class Compteur {
 
         thread1.start();
         thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+
+        System.out.println("The result of thread 1 is: "+ thread1.getResult());
+        System.out.println("The result of thread 2 is: "+ thread2.getResult());
     }
 
 }
---
+++ tp1/Utils.java
@@ -0,0 +1,5 @@
+public class Utils {
+    public static void printSeparator(int exerciseNumber) {
+        System.out.println("\n" + "=".repeat(7) + " Exercice " + exerciseNumber + " " + "=".repeat(7));
+    }
+}    
