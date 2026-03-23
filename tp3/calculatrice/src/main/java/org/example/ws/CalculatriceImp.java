aac8fb10ada6cb0f1d1ec5859f197cd3940b9227
hajarmst211 <hajarmst21@gmail.com>
2026-03-23 15:09:52 +0100

    calculatrice-tp3

---
+++ tp3/calculatrice/src/main/java/org/example/ws/CalculatriceImp.java
@@ -0,0 +1,37 @@
+package org.example.ws;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "org.example.ws.ICalculatrice")
+public class CalculatriceImp implements ICalculatrice {
+
+    @Override
+    public double add(double x, double y) {
+        System.out.println("addition de " + x + " et " + y);
+        return x + y;
+    }
+
+    @Override
+    public double multiply(double x, double y) {
+        System.out.println("multiplication de " + x + " et " + y);
+        return x * y;
+    }
+
+    // --- New Methods ---
+
+    @Override
+    public double subtract(double x, double y) {
+        System.out.println("soustraction de " + x + " et " + y);
+        return x - y;
+    }
+
+    @Override
+    public double divide(double x, double y) throws Exception {
+        if (y == 0) {
+            System.out.println("Erreur: tentative de division par zéro");
+            throw new Exception("Division par zero impossible !");
+        }
+        System.out.println("division de " + x + " par " + y);
+        return x / y;
+    }
+}
